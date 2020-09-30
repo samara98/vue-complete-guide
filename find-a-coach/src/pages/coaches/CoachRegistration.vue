97988f31606950fefdd067732a321ea18bb71b70
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-10-01 06:14:38 +0700

    Find a Coach - Finish

--- find-a-coach/src/pages/coaches/CoachRegistration.vue
+++ find-a-coach/src/pages/coaches/CoachRegistration.vue
@@ -1,11 +1,25 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
+  import CoachForm from '../../components/coaches/CoachForm.vue';
+
   export default {
     name: 'CoachRegistration',
+    components: {
+      CoachForm,
+    },
+    methods: {
+      saveData(data) {
+        this.$store.dispatch('coaches/registerCoach', data);
+        this.$router.replace('/coaches');
+      },
+    },
   };
 </script>
-
-<style></style>
--- find-a-coach/src/store/index.js
+++ find-a-coach/src/store/index.js
@@ -1,8 +1,23 @@
 import { createStore } from 'vuex';
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+import coachesModule from './modules/coaches';
+import requestsModule from './modules/requests';
+
+const store = createStore({
+  modules: {
+    coaches: coachesModule,
+    requests: requestsModule,
+  },
+  state() {
+    return {
+      userId: 'c3',
+    };
+  },
+  getters: {
+    userId(state) {
+      return state.userId;
+    },
+  },
 });
+
+export default store;
